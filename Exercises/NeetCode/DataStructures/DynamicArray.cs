e6040c550d7c88e5ce48761e63e660e80ad970c2
Michal Reclaw <micrec90@gmail.com>
2024-04-28 17:15:27 +0200

    Implemented Design Dynamic Array (Resizable Array)

---
+++ Exercises/NeetCode/DataStructures/DynamicArray.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeetCode.DataStructures
+{
+    public class DynamicArray
+    {
+        private int[] _array;
+        private int _capacity;
+        private int _length;
+
+        public DynamicArray(int capacity)
+        {
+            _capacity = capacity;
+            _length = 0;
+            _array = new int[capacity];
+        }
+
+        public int Get(int i)
+        {
+            return _array[i];
+        }
+
+        public void Set(int i, int n)
+        {
+            _array[i] = n;
+        }
+
+        public void PushBack(int n)
+        {
+            if (_length == _capacity)
+                Resize();
+            _array[_length] = n;
+            _length++;
+        }
+
+        public int PopBack()
+        {
+            _length--;
+            return _array[_length];
+        }
+
+        private void Resize()
+        {
+            _capacity *= 2;
+            int[] newArray = new int[_capacity];
+            for (int i = 0; i < _array.Length; i++)
+                newArray[i] = _array[i];
+            _array = newArray;
+        }
+
+        public int GetSize()
+        {
+            return _length;
+        }
+
+        public int GetCapacity()
+        {
+            return _capacity;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Exercises
-Project containing my solutions for different data structures and programming exercises like LeetCode, HackerRank, Codewars, etc.
+Project containing my solutions for different data structures and programming exercises like LeetCode, HackerRank, Codewars, etc. all in one place.
+## NeetCode
+Implemented solutions for [NeetCode](https://neetcode.io/):
+
+[Design Dynamic Array (Resizable Array)](https://neetcode.io/problems/dynamicArray)
