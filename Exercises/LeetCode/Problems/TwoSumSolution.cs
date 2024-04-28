2a94400998a0991bafa80a535ae8259aeac1b272
Michal Reclaw <micrec90@gmail.com>
2024-04-28 20:08:28 +0200

    Added LeetCode project, solved TwoSum problem

---
+++ Exercises/LeetCode/Problems/TwoSumSolution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public class TwoSumSolution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            for(int i = 0; i < nums.Length; i++)
+            {
+                int num = nums[i];
+                int c = target - num;
+                if (dict.ContainsKey(c))
+                    return new int[] { dict[c], i };
+                dict[num] = i;
+            }
+            return new int[] { };
+        }
+    }
+}
--- README.md
+++ README.md
@@ -6,3 +6,7 @@ Implemented solutions for [NeetCode](https://neetcode.io/):
 [Design Dynamic Array (Resizable Array)](https://neetcode.io/problems/dynamicArray)
 
 [Design Singly Linked List](https://neetcode.io/problems/singlyLinkedList)
+## LeetCode
+Implemented solutions for [LeetCode](https://leetcode.com/):
+
+[1. Two Sum](https://leetcode.com/problems/two-sum/description/)
