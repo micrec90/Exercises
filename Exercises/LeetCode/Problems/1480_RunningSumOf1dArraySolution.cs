3959cfcb2de56501c619a318bbba4fabe766e53e
Michal Reclaw <micrec90@gmail.com>
2025-11-07 14:35:31 +0100

    1480. Running Sum of 1d Array

---
+++ Exercises/LeetCode/Problems/1480_RunningSumOf1dArraySolution.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Problems
+{
+    public class RunningSumOf1dArraySolution
+    {
+        public int[] RunningSum(int[] nums)
+        {
+            int sum = 0;
+            int[] ret = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+                ret[i] = sum;
+            }
+            return ret;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -62,3 +62,5 @@ Tasks whose solutions are implemented for [LeetCode](https://leetcode.com/):
 
 [771. Jewels and Stones](https://leetcode.com/problems/jewels-and-stones/description/)
 
+[1480. Running Sum of 1d Array](https://leetcode.com/problems/running-sum-of-1d-array/description/)
+
