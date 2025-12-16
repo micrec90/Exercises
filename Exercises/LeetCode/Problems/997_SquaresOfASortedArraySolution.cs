7129df7edb15b9127088923052f8757e49739f24
Michal Reclaw <micrec90@gmail.com>
2025-12-16 11:30:46 +0100

    977. Squares of a Sorted Array

---
+++ Exercises/LeetCode/Problems/997_SquaresOfASortedArraySolution.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Problems
+{
+    public class SquaresOfASortedArraySolution
+    {
+        public int[] SortedSquares(int[] nums)
+        {
+            int left = 0;
+            int right = nums.Length - 1;
+            int[] ret = new int[nums.Length];
+
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                if (Math.Abs(nums[left]) > Math.Abs(nums[right]))
+                {
+                    ret[i] = nums[left] * nums[left];
+                    left++;
+                }
+                else
+                {
+                    ret[i] = nums[right] * nums[right];
+                    right--;
+                }
+            }
+            return ret;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -62,5 +62,7 @@ Tasks whose solutions are implemented for [LeetCode](https://leetcode.com/):
 
 [771. Jewels and Stones](https://leetcode.com/problems/jewels-and-stones/description/)
 
+[977. Squares of a Sorted Array](https://leetcode.com/problems/squares-of-a-sorted-array/description/)
+
 [1480. Running Sum of 1d Array](https://leetcode.com/problems/running-sum-of-1d-array/description/)
 
